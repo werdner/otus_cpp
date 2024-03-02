54ef5270a9e6154b67e58804f6b80945b2fa1cd2
werdner <werdner@yandex-team.ru>
2024-03-02 14:50:28 +0300

    the build directiry was deleted

--- build/CMakeFiles/helloworld.dir/lib.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/helloworld.dir/lib.cpp.o: \
-  /Users/werdner/Documents/werdner/otus/lib.cpp \
-  /Users/werdner/Documents/werdner/otus/lib.h \
-  /Users/werdner/Documents/werdner/otus/build/version.h
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/werdner/Documents/werdner/otus
-# Build directory: /Users/werdner/Documents/werdner/otus/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
-set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/Users/werdner/Documents/werdner/otus/CMakeLists.txt;85;add_test;/Users/werdner/Documents/werdner/otus/CMakeLists.txt;0;")
