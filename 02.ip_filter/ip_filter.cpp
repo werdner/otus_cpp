bd9c4e05913f98f8d97693115b04c975154a0d0b
werdner <werdner@yandex-team.ru>
2024-03-29 18:51:51 +0300

    02/ip_filter/memory_leak

--- 02.ip_filter/ip_filter.cpp
+++ 02.ip_filter/ip_filter.cpp
@@ -25,6 +25,7 @@ int main()
             for (int i = 0; i < ip_address_size; ++i)
                 params[i] = IP_Utils::stringToUint8(ip_address[i]); // converse std::string to std::uint8_t
             ip_pool.emplace_back(params);
+            delete [] params;
         }
 
         std::cout << std::endl;
@@ -35,6 +36,7 @@ int main()
         std::uint8_t * filter_1 = IP_Utils::process_params(counter_params, 1); // parse the passed arguments (1)
         std::vector<IP::IpParams> filtred_1 = IP::filter(ip_pool, IP::IpParams(filter_1));
         IP_Utils::print_list(filtred_1);
+        delete [] filter_1;
 
         std::cout << std::endl;
         std::cout << "FILTER_46_70\n\n";
@@ -42,6 +44,7 @@ int main()
         std::uint8_t * filter_46_70 = IP_Utils::process_params(counter_params, 46, 70); // parse the passed arguments (46, 70)
         std::vector<IP::IpParams> filtred_46_70 = IP::filter(ip_pool, IP::IpParams(filter_46_70));
         IP_Utils::print_list(filtred_46_70);
+        delete [] filter_46_70;
 
         std::cout << std::endl;
         std::cout << "FILTER_ANY_46\n\n";
@@ -49,15 +52,13 @@ int main()
         std::uint8_t * filter_any_46 = IP_Utils::process_params(counter_params, 46); // parse the passed arguments (46)
         std::vector<IP::IpParams> filtred_any_46 = IP::filter_any(ip_pool, IP::IpParams(filter_any_46));
         IP_Utils::print_list(filtred_any_46, true);
+        delete [] filter_any_46;
 
         std::cout << std::endl;
         std::cout << "SORT\n\n";
         
         std::sort(begin(ip_pool), end(ip_pool), IP_Utils::compare);
         IP_Utils::print_list(ip_pool);
-
-
-
     }
     catch(const std::exception & e)
     {
