f13772918fba084a56f948aab68e296138881bce
werdner <werdner@yandex-team.ru>
2024-03-30 08:32:05 +0300

    02/ip_filter/new_implementation

--- 02.ip_filter/ip.cpp
+++ 02.ip_filter/ip.cpp
@@ -1,97 +1,90 @@
+#include <array>
+#include <memory>
+#include <vector>
+#include <iostream>
+
 #include "ip.h"
 
-namespace IP {
-    IpParams::IpParams(const std::uint8_t * ip_params) {
-            int num_args = 0;
-            while(ip_params[num_args])
-                ++num_args;
-            if (num_args > Limit)
-                num_args = Limit;
-            if (num_args > 0)
-                params = new std::uint8_t[num_args];
-            for (int i = 0; i < num_args; ++i)
-                params[i] = ip_params[i];
-            size = num_args;  
-    }
+IP::IP_Filter::IP_Filter(const ip_arr & array_ptr, const std::uint8_t input_s) : size(input_s)
+{
+    params = std::make_shared<ip_arr>(array_ptr);
+}
 
-    IpParams::IpParams(IpParams const & ip): IpParams(ip.params) {}
+IP::IP_Filter::~IP_Filter()
+{
+    size = 0;
+}
 
-    void IpParams::operator=(IpParams const & ip)
+IP::IP_Filter::IP_Filter(const IP_Filter & ip_filter) : params(ip_filter.params), size(ip_filter.size) {}
+
+void IP::IP_Filter::operator=(const IP_Filter & ip_filter)
+{
+    params = ip_filter.params;
+    size = ip_filter.size;
+}
+
+const std::vector<IP::ip_arr> IP::filter(const std::vector<IP::ip_arr> input_vac, const IP_Filter & filter_params)
+{
+    std::vector<IP::ip_arr> filtred_vec;
+    const IP::ip_arr * params = filter_params.getRawParams();
+
+    int vec_size = input_vac.size();
+    for (int i = 0; i < vec_size; ++i)
     {
-        int num_args = 0;
-        while(ip.params[num_args])
-            ++num_args;
+        IP::IP_Filter input_ip_params(input_vac[i], 4);
+        int input_ip_params_size = input_ip_params.getSize();
+        int ip_params_size = filter_params.getSize();
+        bool validation_passed = true;
 
-        if (num_args > Limit)
-            num_args = Limit;
-        if (num_args > 0)
-            params = new std::uint8_t[num_args];
-        for (int i = 0; i < num_args; ++i)
-            params[i] = ip.params[i];
-        size = num_args; 
-    }
-
-    std::vector<IpParams> filter(const std::vector<IpParams> & vec, const IpParams & params)
-    {
-        std::vector<IpParams> filtredVec;
-        std::uint8_t * ip_params = params.getParams();
-        
-        int vec_size = vec.size();
-        for (int i = 0; i < vec_size; ++i)
+        for (int k = 0; k < ip_params_size && k < input_ip_params_size; ++k)
         {
-            std::uint8_t * input_ip_params = vec[i].getParams();
-            int input_ip_params_size = vec[i].getSize();
-            int ip_params_size = params.getSize();
-            bool validation_passed = true;
-
-            for (int k = 0; k < ip_params_size && k < input_ip_params_size; ++k)
+            const IP::ip_arr * rawParams = input_ip_params.getRawParams();
+            if ((int)(*params)[k] != (int)(*rawParams)[k])
             {
-                if (ip_params[k] != input_ip_params[k])
-                {
-                    validation_passed = false;
-                    break;
-                }
+                validation_passed = false;
+                break;
             }
-            
-            if (validation_passed)
-                filtredVec.emplace_back(vec[i]);
         }
-
-        return filtredVec;
+            
+        if (validation_passed)
+            filtred_vec.emplace_back(input_vac[i]);
     }
 
-    std::vector<IpParams> filter_any(const std::vector<IpParams> & vec, const IpParams & params)
+    return filtred_vec;
+}
+
+const std::vector<IP::ip_arr> IP::filter_any(const std::vector<IP::ip_arr> input_vac, const IP_Filter & filter_params)
+{
+    std::vector<IP::ip_arr> filtred_vec;
+    const IP::ip_arr * params = filter_params.getRawParams();
+
+    int vec_size = input_vac.size();
+    for (int i = 0; i < vec_size; ++i)
     {
-        std::vector<IpParams> filtredVec;
-        std::uint8_t * ip_params = params.getParams();
+        IP::IP_Filter input_ip_params(input_vac[i], 4);
+        int input_ip_params_size = input_ip_params.getSize();
+        int ip_params_size = filter_params.getSize();
+        bool validation_passed = false;
 
-        int vec_size = vec.size();
-        for (int i = 0; i < vec_size; ++i)
+        for (int k1 = 0, k2 = 0; k1 < input_ip_params_size && k2 < ip_params_size; ++k1)
         {
-            std::uint8_t * input_ip_params = vec[i].getParams();
-            int input_ip_params_size = vec[i].getSize();
-            int ip_params_size = params.getSize();
-            bool validation_passed = false;
-
-            for (int k1 = 0, k2 = 0; k1 < input_ip_params_size && k2 < ip_params_size; ++k1)
+            const IP::ip_arr * rawParams = input_ip_params.getRawParams();
+            if (validation_passed && (int)(*params)[k2] != (int)(*rawParams)[k1])
             {
-                if (validation_passed && ip_params[k2] != input_ip_params[k1])
-                {
-                    validation_passed = false;
-                    break;
-                }
-
-                if (ip_params[k2] == input_ip_params[k1])
-                {
-                    ++k2;
-                    validation_passed = true;
-                }
+                validation_passed = false;
+                break;
             }
-            
-            if (validation_passed)
-                filtredVec.emplace_back(vec[i]);
-        }
 
-        return filtredVec;
+            if ((int)(*params)[k2] == (int)(*rawParams)[k1])
+            {
+                ++k2;
+                validation_passed = true;
+            }
+        }
+            
+        if (validation_passed)
+            filtred_vec.emplace_back(input_vac[i]);
     }
+
+    return filtred_vec;
 }
--- 02.ip_filter/ip_filter.cpp
+++ 02.ip_filter/ip_filter.cpp
@@ -3,64 +3,65 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <array>
+#include <memory>
+#include <cstdint>
 
-#include "ip.h"
 #include "utils.h"
+#include "ip.h"
 
-int main()
+int main(int argc, char const *argv[])
 {
     try
     {
-        std::vector<IP::IpParams > ip_pool;
+        std::vector<IP::ip_arr> ip_pool;
 
         for(std::string line; std::getline(std::cin, line);)
         {
             std::vector<std::string> v = IP_Utils::split(line, '\t');
             std::vector<std::string> ip_address = IP_Utils::split(v.at(0), '.');
-            
-            int ip_address_size = ip_address.size();
-            std::uint8_t * params = new std::uint8_t[ip_address_size];
+            std::array<std::uint8_t, IP::IP_Filter::LIMIT> array;
+            for (int i = 0; i < 4; ++i)
+            {
+                array[i] = IP_Utils::stringToUint8(ip_address[i]);
+            }
 
-            for (int i = 0; i < ip_address_size; ++i)
-                params[i] = IP_Utils::stringToUint8(ip_address[i]); // converse std::string to std::uint8_t
-            ip_pool.emplace_back(params);
-            delete [] params;
+            ip_pool.push_back(array);
         }
+
 
         std::cout << std::endl;
         std::cout << "FILTER_1\n\n";
 
-        int counter_params = 0;
+        const IP::IP_Filter filter_params_1 = IP::IP_Filter(IP::ip_arr{1}, 1);
+        const std::vector<IP::ip_arr> filter_1 = IP::filter(ip_pool, filter_params_1);
 
-        std::uint8_t * filter_1 = IP_Utils::process_params(counter_params, 1); // parse the passed arguments (1)
-        std::vector<IP::IpParams> filtred_1 = IP::filter(ip_pool, IP::IpParams(filter_1));
-        IP_Utils::print_list(filtred_1);
-        delete [] filter_1;
+        IP_Utils::print_ip<IP::IP_Filter::LIMIT>(filter_1);
 
         std::cout << std::endl;
         std::cout << "FILTER_46_70\n\n";
 
-        std::uint8_t * filter_46_70 = IP_Utils::process_params(counter_params, 46, 70); // parse the passed arguments (46, 70)
-        std::vector<IP::IpParams> filtred_46_70 = IP::filter(ip_pool, IP::IpParams(filter_46_70));
-        IP_Utils::print_list(filtred_46_70);
-        delete [] filter_46_70;
+        const IP::IP_Filter filter_params_46_70 = IP::IP_Filter(IP::ip_arr{46, 70}, 2);
+        const std::vector<IP::ip_arr> filter_46_70 = IP::filter(ip_pool, filter_params_46_70);
+
+        IP_Utils::print_ip<IP::IP_Filter::LIMIT>(filter_46_70);
 
         std::cout << std::endl;
         std::cout << "FILTER_ANY_46\n\n";
 
-        std::uint8_t * filter_any_46 = IP_Utils::process_params(counter_params, 46); // parse the passed arguments (46)
-        std::vector<IP::IpParams> filtred_any_46 = IP::filter_any(ip_pool, IP::IpParams(filter_any_46));
-        IP_Utils::print_list(filtred_any_46, true);
-        delete [] filter_any_46;
+        const IP::IP_Filter filter_params_any_46 = IP::IP_Filter(IP::ip_arr{46}, 1);
+        const std::vector<IP::ip_arr> filter_any_46 = IP::filter_any(ip_pool, filter_params_any_46);
+
+        IP_Utils::print_ip<IP::IP_Filter::LIMIT>(filter_any_46);
 
         std::cout << std::endl;
-        std::cout << "SORT\n\n";
-        
-        std::sort(begin(ip_pool), end(ip_pool), IP_Utils::compare);
-        IP_Utils::print_list(ip_pool);
+        std::cout << "LEXICOGRAPHICAL ORDER\n\n";
+
+        std::sort(begin(ip_pool), end(ip_pool), [](const IP::ip_arr & arr1, const IP::ip_arr & arr2){ return arr1 > arr2; });
+        IP_Utils::print_ip<IP::IP_Filter::LIMIT>(ip_pool);
+
     }
-    catch(const std::exception & e)
+    catch(const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
